d0795ed531a6bf90b8e5370892119309733fd61c
fengyunkai <fyk943749465@gmail.com>
2022-04-23 22:51:32 -0700

    redis 发布订阅消息

---
+++ src/main/java/com/redis/demo/config/RedisListenerConfig.java
@@ -0,0 +1,21 @@
+package com.redis.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisListenerConfig {
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+                                            MessageListenerAdapter listenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic("redis-test-topic"));
+        return container;
+    }
+}
